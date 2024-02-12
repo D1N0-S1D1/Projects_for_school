c88f0ffdfcd0f5e8749043753a2babcbadd2a0f1
D1N0-S1D1 <dmytro__shevchuk@ukr.net>
2024-02-12 16:32:21 +0200

    12.02.2024

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+# By Shevchuk Dmytro 8-B (D1N0)
 
-#By Shevchuk Dmytro 8-B (D1N0)
+from tkinter.tix import Tk
 
-import tkinter as tk
 
-root = tk.Tk()
-root.geometry('400x350')
-root.title('Shevchuk')
-root['bg'] = 'red'
+def click(event):
+    root['bg'] = 'green'
+    root.title('Змінення значень властивостей вікна')
+    root.geometry('400x200+500+70')
+
+
+root = Tk()
+root.bind('<1>', click)
 root.mainloop()
 
-def get_compiled_for():
+
+def get_compiled_for(коментарій):
     """Teacher"""
