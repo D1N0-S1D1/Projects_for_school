b6e52b168eb2660277cd971af5b029e2942cae02
D1N0-S1D1 <dmytro__shevchuk@ukr.net>
2024-02-12 16:46:41 +0200

    12.02.2024 16:45

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 # By Shevchuk Dmytro 8-B (D1N0)
 
-from tkinter.tix import Tk
+from tkinter import *
 
 
 def click(event):
-    root['bg'] = 'green'
-    root.title('Змінення значень властивостей вікна')
-    root.geometry('400x200+500+70')
 
+    a = root.winfo_y() + 50
+    b = root.winfo_width() + 40
+    root.geometry(f'480x{a}+{b}+200')
 
 root = Tk()
+root.geometry('480x320+200+200')
 root.bind('<1>', click)
 root.mainloop()
 
 
-def get_compiled_for(коментарій):
+def get_compiled_for(скомпільовано_для):
     """Teacher"""
